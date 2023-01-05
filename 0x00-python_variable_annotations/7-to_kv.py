eb8e78899f34cc2ce1dc05c0c1f337622403ffa8
Desire Barine <desirebarine16@gmail.com>
2023-01-05 22:31:49 +0100

    changed types

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """type-annotated function to_kv"""
-from typing import Union,  List, Tuple
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: List[Union[int, float]]) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """string k and an int OR float v and returns a tuple"""
     return (k, v ** 2)
